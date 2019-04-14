8da87becafd44dbaac8c05c5823c4d1cccf32153
iAfolayan <iafolayanibikunle@gmail.com>
2019-04-14 17:27:13 +0100

    ft(landing): implement landing page component
    - landing page component
    - configure routes
    - set up 404 page
    - set up footer component
    - set up navbar component
    
    [Finishes]

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import NavBar from '../../components/NavBar';
+import PropTypes from 'prop-types';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+      </div>
+    );
+  }
+}
+
+Dashboard.propTypes = {
+
+};
+
+export default Dashboard;
