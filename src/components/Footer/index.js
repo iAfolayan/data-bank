40eb2d779a51a99adcaddd53218a636eb4bd2e56
iAfolayan <iafolayanibikunle@gmail.com>
2019-04-14 23:13:02 +0100

    ft(footer): implement foot component
    - footer component
    
    [Finished]

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Footer.scss';
+import { Segment, List } from 'semantic-ui-react';
 
 const Footer = () => (
-  <div className="footer">
-    <h1>Footer</h1>
-  </div>
+  <Segment className='footer'>
+    <div className="copyright">&copy; Diocese of Kwara</div>
+    <div className="designer">
+      <List horizontal link>
+        <List.Item as='dashboard'>Dashboard</List.Item>
+        <List.Item as='feedback'>Feedback</List.Item>
+      </List>
+    </div>
+  </Segment>
 )
 
 export default Footer;
